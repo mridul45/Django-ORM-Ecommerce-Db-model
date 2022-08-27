275e5c69b4a0689d0c94cd5f6f8aaed0dbb9cf54
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-08-27 23:37:22 +0530

    Add files via upload

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-	return render(request,'home.html')
+	objs = Product.objects.all()
+	context = {"objs":objs}
+
+	return render(request,"index.html")
