a08a1cbe89594adbf288060e3f41ba5a64b248ef
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-08-28 12:28:35 +0530

    Add files via upload

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,48 @@
 from django.db import models
+from django.contrib.auth.admin import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+# class PersonManagerInactive(models.Manager):
+#     def get_queryset(self):
+#         return super(PersonManagerInactive,self).get_queryset.filter(is_active=False)
+
+# class PersonManagerActive(models.Manager):
+#     def get_queryset(self):
+#         return super(PersonManagerActive,self).get_queryset.filter(is_active=True)
+
+# class Person(User):
+#     inactive = PersonManagerInactive()
+#     class Meta:
+#         proxy = True
+#         ordering = ("first_name",)
+
+#     @classmethod
+#     def count_all(cls,):
+#         return cls.object.filter(is_active=True).count()
+
+#     def check_active(self):
+#         if self.is_active == True:
+#             return "You are active"
+
+#         else:
+#             return "You are not Active"
+
+#     def __str__(self):
+#         return self.first_name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    age = models.IntegerField(null=True, blank=True)
+    nickname = models.CharField(max_length=100,null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
 
 
 class Category(models.Model):
